5b9e899234580030d9234a887d33e6f98d01ecf0
jaLeague <jackie.arseneau@jointheleague.org>
2019-01-15 17:39:48 -0800

    Update HeroFight.java
    Fix typos

--- src/hero/HeroFight.java
+++ src/hero/HeroFight.java
@@ -3,10 +3,10 @@ package hero;
 public class HeroFight {
 	/*
 	 * A hero is on his way to the castle to complete his mission. However, he's
-	 * been told that the castle is surrounded with a couple of powerful dragons!
-	 * each dragon takes 2 bullets to be defeated, our hero has no idea how many
+	 * been told that the castle is surrounded by some powerful dragons!
+	 * Each dragon takes 2 bullets to be defeated, but our hero has no idea how many
 	 * bullets he should carry.. Assuming he's gonna grab a specific given number of
-	 * bullets and move forward to fight another specific given number of dragons,
+	 * bullets and move forward to fight a specific given number of dragons,
 	 * will he survive? Return True if yes, False otherwise :)
 	 */
 	public static boolean hero(int bullets, int dragons) {
